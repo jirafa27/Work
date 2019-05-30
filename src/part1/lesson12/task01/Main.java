bc05ec6cb0d3e81c2be00528432737e36724c388
jirafa27 <jirafa27@mail.ru>
2019-05-30 12:45:18 +0300

    1

--- src/part1/lesson12/task01/Main.java
+++ src/part1/lesson12/task01/Main.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Object> obj = new ArrayList<>();
+        Object o;
         for (int i = 0; i < 1000000000; i++) {
+            o = new Object();
             if (i%3==0)
-                obj.add(new Object());
+                obj.add(o);
         }
     }
 }
