9cd18090542be4a6a637b68d8403042ca6cab28d
jirafa27 <jirafa27@mail.ru>
2019-05-30 12:39:42 +0300

    1

--- src/part1/lesson12/task01/Main.java
+++ src/part1/lesson12/task01/Main.java
@@ -1,10 +1,13 @@
 package part1.lesson12.task01;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Object obj;
+        ArrayList<Object> obj = new ArrayList<>();
         for (int i = 0; i < 1000000000; i++) {
-            obj = new Object();
+            if (i%3==0)
+                obj.add(new Object());
         }
     }
 }
